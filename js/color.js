da271a3d107e5f7ecae8e762a94cef37f1120e86
tyrale <tyrale@tyrales-mbp.hsd1.tn.comcast.net>
2013-11-25 21:47:13 -0600

    added project section

--- js/color.js
+++ js/color.js
@@ -1,27 +1,35 @@
 $('.color')
     .delay(10)
     .queue(function(){
-      $(this).css({"transition": "background-color 10s"});
+      $(this).css({"transition": "background-color 20s"});
+        $(this).dequeue();
+    });
+$('.font-color')
+    .delay(10)
+    .queue(function(){
+      $(this).css({"transition": "color 20s"});
         $(this).dequeue();
     });
 $('.shadow')
     .delay(10)
     .queue(function(){
-      $(this).css({"transition": "text-shadow 10s , color 10s"});
+      $(this).css({"transition": "text-shadow 20s , color 20s"});
         $(this).dequeue();
     });
 
 $obj = $('.color');
+$obj1 = $('.font-color');
 $obj2 = $('.shadow');
 
 var setHue = Math.round( Math.random() * 360 );
 var setSat = Math.round( Math.random() * 70 + 5);
-var setLight = Math.round( Math.random() * 60 + 5);
+var setLight = Math.round( Math.random() * 20 + 20);
 
 var newColor = "hsl(" + setHue + ", " + setSat + "%, " + setLight + "%)";
 var shadowColor = "hsl(" + setHue + ", " + setSat + "%, " + (setLight - 10)+ "%)";
 
 $obj.css({ "background-color": newColor });
+$obj1.css({ "color": newColor });
 $obj2.css({ "color": shadowColor });
   //console.log(newColor);
 
@@ -33,6 +41,7 @@ setInterval( function () {
   var newColor = "hsl(" + setHue + ", " + setSat + "%, " + setLight + "%)";
   var shadowColor = "hsl(" + setHue + ", " + setSat + "%, " + (setLight - 10)+ "%)";
   $obj.css({ "background-color": newColor });
+  $obj1.css({ "color": newColor });
   $obj2.css({ "color": shadowColor });
   //console.log(newColor);
 }, 8000);
